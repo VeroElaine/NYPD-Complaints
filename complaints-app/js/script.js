e4c883420f27b6d54d12683563cde37e331a6951
Veronica Ramirez <55036002+VeroElaine@users.noreply.github.com>
2020-05-11 15:50:19 -0700

    reorganizing files

---
+++ complaints-app/js/script.js
@@ -0,0 +1,38 @@
+// Constants
+
+const baseUrl = "https://data.cityofnewyork.us/resource/erm2-nwe9.json"
+
+// State Variables
+
+let limit, complaints, borough;
+
+// Cached element references
+const $yearEl = $("#year");
+const $controls = $("#controls");
+const $inputEl = $("input[type=number]");
+const $complaintListEl = $("#complaint-list");
+
+// Event Listeners
+
+$controls.on("click", "button", handleGetData)
+
+// Functions
+init(); 
+
+function init() {
+    $yearEl.html(new Date().getFullYear());
+    complaints = [];
+}
+
+function handleGetData(evt) {
+    borough = evt.target.dataset.borough;
+    limit = $inputEl.val() || "10";
+
+    $.ajax({
+        url: baseUrl + "?agency=NYPD&$limit=" + limit + "&borough=" + borough
+    }).then(function(data){
+        console.log(data)
+    }, function(error) {
+        console.log(error)
+    }); 
+}
